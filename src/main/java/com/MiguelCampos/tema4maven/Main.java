cefa282e9daeac41473f1b3bfe25fcc11d2ee778
miguel10067 <miguelcampara1@gmail.com>
2026-02-06 18:35:03 +0100

    Automatizacion con Maven

--- src/main/java/com/MiguelCampos/tema4maven/Main.java
+++ src/main/java/com/MiguelCampos/tema4maven/Main.java
@@ -1,6 +1,15 @@
 package com.MiguelCampos.tema4maven;
+
+import com.github.lalyos.jfiglet.FigletFont;
+
+import java.io.IOException;
+
 public class Main {
-    static void main() {
+    static void main() throws IOException {
+    //crear baner
+        String texto = "Miguel Campos";
+        String banner = FigletFont.convertOneLine(texto);
+        System.out.println(banner);
 
     }
 }
